96dfdd517cef93af8c3dcadb00368401a74013b4
skaftisveins <skaftisveins@gmail.com>
2021-05-15 13:16:21 +0000

    Added MIMEText and Header. Added encoding on letter_file. Sending emails with utf-8 encoding. Seems to work now!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 from datetime import datetime
 from random import randint
 import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -14,12 +16,17 @@ if (today_tuple in birthday_dict):
     birthday_person = birthday_dict[today_tuple]
     file_path = f"letter_templates/letter_{randint(1, 3)}.txt"
 
-    with open(file_path, mode="r") as letter_file:
+    with open(file_path, mode="r", encoding="utf-8") as letter_file:
         contents = letter_file.read()
-        contents = contents.replace(PLACEHOLDER, birthday_person["name"])
+        content = contents.replace(PLACEHOLDER, birthday_person["name"])
+        
+        msg = MIMEText(content, 'plain', 'utf-8')
+        msg["Subject"] = Header("Til Hamingju Med Afmælið!", 'utf-8')
+        msg["From"] = MY_EMAIL
+        msg["To"] = birthday_person["email"]
 
         with smtplib.SMTP(SMTP_ADDRESS) as connection:
             connection.starttls()
             connection.login(MY_EMAIL, MY_PASSWORD)
             connection.sendmail(
-                from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"Subject:Til Hamingju Med Afmaelid!\n\n{contents}")
+                msg["From"], msg["To"], msg.as_string())
